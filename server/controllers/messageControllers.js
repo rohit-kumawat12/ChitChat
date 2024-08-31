1ed0c462f48ffe17a90f421740ad6bcd3d4d1a74
rohit-kumawat12 <rohitkumawat.ak@gmail.com>
2024-08-31 15:58:08 +0530

    Create API for Single and Group Message

--- server/controllers/messageControllers.js
+++ server/controllers/messageControllers.js
@@ -1,7 +1,54 @@
 const asyncHandler = require("express-async-handler");
+const Message = require("../Models/messageModel");
+const Chat = require("../Models/chatModel");
+const User = require("../Models/userModel");
 
 const sendMessage = asyncHandler(async(req, res)=>{
+    const {content, chatId} = req.body;
+    if(!content || !chatId){
+        console.log("Invalid data passed into request");
+        return res.status(400);
+    }
 
+    var newMessage = {
+        sender:req.user._id,
+        content:content,
+        chat:chatId,
+    }
+
+    try{
+        var message = await  Message.create(newMessage);
+
+        message = await message.populate("sender","name pic");
+        message = await message.populate("chat");
+        message = await User.populate(message,{
+            path:"sender",
+            select:"name pic eamil"
+        });
+        
+        await Chat.findByIdAndUpdate(req.body.chatId,{
+            latestMessage: message,
+        });
+
+        res.json(message);
+
+    }catch(error){
+        res.status(400);
+        throw new Error(error.message);
+    }
 });
 
-module.exports = {sendMessage};
+const allMessages = asyncHandler(async (req, res) => {
+    try{
+        const message = await Message.find({chat: req.params.chatId}).populate(
+            "sender",
+            "name pic email"
+        );
+        res.json(message);
+    }catch(error){
+        res.status(400);
+        throw new Error(error.message);
+    }
+});
+
+module.exports = {sendMessage, allMessages};
--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const { chats } = require("./data/data");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
+const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require('./middleware/errorMiddleware');
 
 const app = express();
@@ -22,6 +23,7 @@ app.get("/",(req,res)=>{
 
 app.use('/api/user',userRoutes);
 app.use('/api/chat',chatRoutes);
+app.use('/api/message',messageRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
